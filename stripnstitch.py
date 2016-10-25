34edb8c748e73b11c59f40137e6b59cc299d6452
amon <amon@nandynarwhals.org>
2016-10-25 20:17:18 +0800

    Initial scripts and samples.

---
+++ stripnstitch.py
@@ -0,0 +1,32 @@
+from PIL import Image
+
+class StripNStitcher:
+
+    def __init__(self, filename):
+        self.image = Image.open(filename)
+
+    def strip(self, position, width=1):
+        tempim = self.image.crop((position, 0, position+width,
+                                  self.image.height))
+        return tempim
+
+    def stitch(self, tempim):
+        newim = Image.new(self.image.mode, (self.image.height,)*2)
+        data = []
+        for i in tempim.getdata():
+            for j in range(tempim.height):
+                data.append(i)
+        newim.putdata(data)
+        return newim
+
+    def stripnstitch(self, position):
+        simage = self.strip(position)
+        nimage = self.stitch(simage)
+        return nimage
+
+def main():
+    sns = StripNStitcher("./samples/pepe.png")
+    sns.stripnstitch(250).show()
+
+if __name__  == "__main__":
+    main()
